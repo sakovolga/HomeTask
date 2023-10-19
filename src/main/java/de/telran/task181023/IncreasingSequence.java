fcaf0715d0b843426204c474d2e42e4109328af4
sakovolga <sakovolga@live.com>
2023-10-19 19:24:21 +0200

    added homework for 18/10/2023 about ReplacedIndexesByZero  and IncreasingSequence

---
+++ src/main/java/de/telran/task181023/IncreasingSequence.java
@@ -0,0 +1,36 @@
+package de.telran.task181023;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class IncreasingSequence {
+    public static void main(String[] args) {
+        int[] arrInt = getArrInt();
+        System.out.println("Массив из 5 случайных чисел " + Arrays.toString(arrInt));
+        System.out.println("Это строго возрастающая последовательность? " + isSequenceIncreasing(arrInt));
+    }
+    private static boolean isSequenceIncreasing(int[] arrInt) {
+        boolean bool = false;
+       for (int i = 0; i < arrInt.length-1; i++) {
+            if (arrInt[i] > arrInt[i + 1]){
+                bool =  false;
+                break;
+            }
+            else {
+                bool =  true;
+            }
+        }
+       return bool;
+    }
+    private static int[] getArrInt() {
+        Random random = new Random();
+        int[] arrInt = new int[5];
+        for(int i = 0; i < arrInt.length; i++){
+            arrInt[i] = random.nextInt(90) + 10;
+        }
+        return arrInt;
+    }
+}
+//    Создайте массив из 5 случайных целых чисел из интервала [10;99]
+//        Выведите его на консоль в строку.
+//        Определите и выведите на экран сообщение о том, является ли массив строго возрастающей последовательностью.
---
+++ src/main/java/de/telran/task181023/ReplacedIndexesByZero.java
@@ -0,0 +1,32 @@
+package de.telran.task181023;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class ReplacedIndexesByZero {
+    public static void main(String[] args) {
+        int[] arrInt = getInts(8);
+        System.out.println("Первоначальный массив " + Arrays.toString(arrInt));
+        System.out.println("Новый массив " + Arrays.toString(replaceToZero(arrInt)));
+    }
+    private static int[] replaceToZero(int[] arrInt) {
+        for(int i = 0; i < arrInt.length; i++){
+            if (i % 2 == 0){
+                arrInt[i] = 0;
+            }
+        }
+        return arrInt;
+    }
+    private static int[] getInts(int arrLenght) {
+        Random random = new Random();
+        int[] arrInt = new int[arrLenght];
+        for(int i = 0; i < arrInt.length; i++){
+            arrInt[i] = random.nextInt(50) + 1;
+        }
+        return arrInt;
+    }
+}
+//    Создайте массив из 8 случайных целых чисел из интервала [1;50]
+//        Выведите массив на консоль в строку.
+//        Замените каждый элемент с нечетным индексом на ноль.
+//        Снова выведете массив на консоль в отдельной строке.
